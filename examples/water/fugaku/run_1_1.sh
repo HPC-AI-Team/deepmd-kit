9c8270d5adc3a35ad06eee9cc271df958630f445
gzq <gzq9425@qq.com>
2021-07-06 14:55:41 +0900

    multi node test
    Former-commit-id: c1cc65b5082f52f007ea423615ead15c22b74007
    Former-commit-id: ae7364dfc73929ba002f52df2fedaabc574a65f6
    Former-commit-id: ee1efd41658c2b68d7534bada19d5634d836e204

--- examples/water/fugaku/run_1_1.sh
+++ examples/water/fugaku/run_1_1.sh
@@ -3,7 +3,6 @@
 #PJM -L  "freq=2200"                         
 #PJM -L "rscgrp=small"         # Specify resource group
 #PJM -L  "elapse=00:05:00"                 # Elapsed time limit 1 hour
-#PJM --mpi "shape=1"
 #PJM --mpi "max-proc-per-node=48"          # Maximum number of MPI processes created per node
 #PJM -s                                    # Statistical information output
 
--- examples/water/fugaku/run_compress_preprocess_48_192.sh
+++ examples/water/fugaku/run_compress_preprocess_48_192.sh
@@ -2,8 +2,7 @@
 #PJM -L  "node=1"                          # Number of assign node 8 (1 dimention format)
 #PJM -L  "freq=2200"                         
 #PJM -L "rscgrp=small"                     # Specify resource group
-#PJM -L  "elapse=00:5:00"                 # Elapsed time limit 1 hour
-#PJM --mpi "shape=1"
+#PJM -L  "elapse=00:02:00"                 # Elapsed time limit 1 hour
 #PJM --mpi "max-proc-per-node=48"          # Maximum number of MPI processes created per node
 #PJM -s                                    # Statistical information output
 
@@ -20,4 +19,4 @@ export TF_INTER_OP_PARALLELISM_THREADS=1
 export HAVE_PREPROCESSED=1
 export TF_CPP_MIN_LOG_LEVEL=3
 
-mpirun -np 48 lmp_mpi -echo screen -in ./in.water_compress_preprocess_192
+mpiexec lmp_mpi -echo screen -in ./in.water_compress_preprocess_192
