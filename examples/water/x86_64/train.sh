39de7cca8da33dc940fd404f18e359476de44037
gzq <gzq9425@qq.com>
2021-07-09 22:23:44 +0800

    update model
    Former-commit-id: 3cee4c16e7e75b65edb2cfe464f8fc0f5e3c78c6
    Former-commit-id: 509f2d1523dd89b156268851cb0760fbbafaef79
    Former-commit-id: a9e048bdc24d39db5453027ca9c95d30b0f074b4

--- examples/water/x86_64/train.sh
+++ examples/water/x86_64/train.sh
@@ -6,7 +6,6 @@ export deepmd_root=$HOME/deepmd-kit
 source $deepmd_root/script/x86_64/env.sh
 bash $deepmd_root/script/x86_64/build_deepmd.sh
 
-dp train ../se_e2_a/input_double_1000.json
-dp freeze -o ../model/double/original/graph-original-gemm_tanh_fusion.pb
-dp test -m ../model/double/original/graph-original-gemm_tanh_fusion.pb -s ../data/data_3 -n 1
-
+dp train ../se_e2_a/input_float.json
+dp freeze -o ../model/float/graph.pb
+dp test -m ../model/float/graph.pb -s ../data/data_3 -n 1
--- examples/water/x86_64/transfer_param.sh
+++ examples/water/x86_64/transfer_param.sh
@@ -5,17 +5,17 @@ source $deepmd_root/script/x86_64/env.sh
 bash $deepmd_root/script/x86_64/build_deepmd.sh
 
 set -x
-cp ../model/double/graph.pb ../model/double/original/graph-original-baseline.pb
-dp transfer -O ../model/double/graph.pb -r ../model/double/original/graph-original-gemm.pb -o ../model/double/original/graph-original-gemm.pb
-dp transfer -O ../model/double/graph.pb -r ../model/double/original/graph-original-gemm_tanh.pb -o ../model/double/original/graph-original-gemm_tanh.pb
+# cp ../model/double/graph.pb ../model/double/original/graph-original-baseline.pb
+# dp transfer -O ../model/double/graph.pb -r ../model/double/original/graph-original-gemm.pb -o ../model/double/original/graph-original-gemm.pb
+# dp transfer -O ../model/double/graph.pb -r ../model/double/original/graph-original-gemm_tanh.pb -o ../model/double/original/graph-original-gemm_tanh.pb
 dp transfer -O ../model/double/graph.pb -r ../model/double/original/graph-original-gemm_tanh_fusion.pb -o ../model/double/original/graph-original-gemm_tanh_fusion.pb
 
-dp compress ../se_e2_a/input_double.json -i ../model/double/original/graph-original-baseline.pb -o ../model/double/compress/graph-compress-baseline.pb
-dp compress ../se_e2_a/input_double.json -i ../model/double/original/graph-original-gemm.pb -o ../model/double/compress/graph-compress-gemm.pb
-dp compress ../se_e2_a/input_double.json -i ../model/double/original/graph-original-gemm_tanh.pb -o ../model/double/compress/graph-compress-gemm_tanh.pb
+# dp compress ../se_e2_a/input_double.json -i ../model/double/original/graph-original-baseline.pb -o ../model/double/compress/graph-compress-baseline.pb
+# dp compress ../se_e2_a/input_double.json -i ../model/double/original/graph-original-gemm.pb -o ../model/double/compress/graph-compress-gemm.pb
+# dp compress ../se_e2_a/input_double.json -i ../model/double/original/graph-original-gemm_tanh.pb -o ../model/double/compress/graph-compress-gemm_tanh.pb
 dp compress ../se_e2_a/input_double.json -i ../model/double/original/graph-original-gemm_tanh_fusion.pb -o ../model/double/compress/graph-compress-gemm_tanh_fusion.pb
 
-python /data/home/guozhuoqiang/deepmd-kit/_skbuild/linux-x86_64-3.7/cmake-install/deepmd/entrypoints/preprocess.py ../model/double/compress/graph-compress-baseline.pb ../model/double/compress-preprocess/graph-compress-preprocess-baseline.pb
-python /data/home/guozhuoqiang/deepmd-kit/_skbuild/linux-x86_64-3.7/cmake-install/deepmd/entrypoints/preprocess.py ../model/double/compress/graph-compress-gemm.pb ../model/double/compress-preprocess/graph-compress-preprocess-gemm.pb
-python /data/home/guozhuoqiang/deepmd-kit/_skbuild/linux-x86_64-3.7/cmake-install/deepmd/entrypoints/preprocess.py ../model/double/compress/graph-compress-gemm_tanh.pb ../model/double/compress-preprocess/graph-compress-preprocess-gemm_tanh.pb
+# python /data/home/guozhuoqiang/deepmd-kit/_skbuild/linux-x86_64-3.7/cmake-install/deepmd/entrypoints/preprocess.py ../model/double/compress/graph-compress-baseline.pb ../model/double/compress-preprocess/graph-compress-preprocess-baseline.pb
+# python /data/home/guozhuoqiang/deepmd-kit/_skbuild/linux-x86_64-3.7/cmake-install/deepmd/entrypoints/preprocess.py ../model/double/compress/graph-compress-gemm.pb ../model/double/compress-preprocess/graph-compress-preprocess-gemm.pb
+# python /data/home/guozhuoqiang/deepmd-kit/_skbuild/linux-x86_64-3.7/cmake-install/deepmd/entrypoints/preprocess.py ../model/double/compress/graph-compress-gemm_tanh.pb ../model/double/compress-preprocess/graph-compress-preprocess-gemm_tanh.pb
 python /data/home/guozhuoqiang/deepmd-kit/_skbuild/linux-x86_64-3.7/cmake-install/deepmd/entrypoints/preprocess.py ../model/double/compress/graph-compress-gemm_tanh_fusion.pb ../model/double/compress-preprocess/graph-compress-preprocess-gemm_tanh_fusion.pb
