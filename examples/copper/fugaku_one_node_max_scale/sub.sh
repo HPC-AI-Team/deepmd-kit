05f8b1e88043be2c3c8a55882423d649dfdbd89c
gzq <gzq9425@qq.com>
2021-07-30 10:09:57 +0900

    single node copper
    Former-commit-id: 08f6c3348682ef1804157d03cba1e4f63342c31c
    Former-commit-id: 0ffd22c8d189263e3919aaad0b106e22fd5dcd17
    Former-commit-id: bca781bd2539ba105e6cf5567d407a128b227bf5
    Former-commit-id: 40fe64622a65e88fb03b4c5a7e7e78e3692930a0
    Former-commit-id: dffc6907991633ac226e42a6315a52d86b1e7c9b
    Former-commit-id: 17d0c53d326e7a5ba6face6929eca119b54a3198

---
+++ examples/copper/fugaku_one_node_max_scale/sub.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# pjsub run_compress_preprocess_4x12x1_288.sh
+# pjsub run_compress_preprocess_4x12x1_576.sh
+# pjsub run_compress_preprocess_4x12x1_624.sh
+# pjsub run_compress_preprocess_4x12x1_672.sh
+# pjsub run_compress_preprocess_4x12x1_720.sh
+# pjsub run_compress_preprocess_4x12x1_768.sh
+# pjsub run_compress_preprocess_4x12x1_816.sh
+# pjsub run_compress_preprocess_4x12x1_864.sh
+
+
+# pjsub run_compress_preprocess_8x6x1_576.sh
+# pjsub run_compress_preprocess_8x6x1_624.sh
+# pjsub run_compress_preprocess_8x6x1_672.sh
+# pjsub run_compress_preprocess_8x6x1_720.sh
+# pjsub run_compress_preprocess_8x6x1_768.sh
+# pjsub run_compress_preprocess_8x6x1_816.sh
+# pjsub run_compress_preprocess_8x6x1_864.sh
+# pjsub run_compress_preprocess_8x6x1_912.sh
+# pjsub run_compress_preprocess_8x6x1_960.sh
+# pjsub run_compress_preprocess_8x6x1_1008.sh
+# pjsub run_compress_preprocess_8x6x1_1056.sh
+# pjsub run_compress_preprocess_8x6x1_1104.sh
+# pjsub run_compress_preprocess_8x6x1_1152.sh
+
+
+# pjsub run_compress_preprocess_12x4x1_576.sh
+# pjsub run_compress_preprocess_12x4x1_864.sh
+# pjsub run_compress_preprocess_12x4x1_912.sh
+# pjsub run_compress_preprocess_12x4x1_960.sh
+# pjsub run_compress_preprocess_12x4x1_1008.sh
+
+
+# pjsub run_compress_preprocess_16x3x1_576.sh
+# pjsub run_compress_preprocess_16x3x1_624.sh
+# pjsub run_compress_preprocess_16x3x1_672.sh
+# pjsub run_compress_preprocess_16x3x1_720.sh
+# pjsub run_compress_preprocess_16x3x1_768.sh
+# pjsub run_compress_preprocess_16x3x1_816.sh
+# pjsub run_compress_preprocess_16x3x1_864.sh
+
+# pjsub run_compress_preprocess_24x2x1_576.sh
+# pjsub run_compress_preprocess_24x2x1_864.sh
+# pjsub run_compress_preprocess_24x2x1_912.sh
+# pjsub run_compress_preprocess_24x2x1_960.sh
+# pjsub run_compress_preprocess_24x2x1_1008.sh
+
+# pjsub run_compress_preprocess_48x1x1_576.sh
+
+pjsub run_compress_preprocess_48x1x1_624.sh
+pjsub run_compress_preprocess_48x1x1_672.sh
+pjsub run_compress_preprocess_48x1x1_720.sh
+pjsub run_compress_preprocess_48x1x1_768.sh
+pjsub run_compress_preprocess_48x1x1_816.sh
+
+# pjsub run_compress_preprocess_48x1x1_864.sh
