9b5b517fafec679d7b0f78db393d6aa9fcda80b3
gzq <gzq9425@qq.com>
2021-07-30 11:51:52 +0900

    water strong scale small
    Former-commit-id: c5af326d8dc02f4ad31460cd5831e9393467f444
    Former-commit-id: 3e8ce87fc523ebf3c054ef26202100420b078a25
    Former-commit-id: 1cfc909dd0045fccc3cca904b4c67e6d6344974b
    Former-commit-id: 88899945c1f7ee6daced515dfb6a8ec8593e3001
    Former-commit-id: 75ca1ca2864f2432d8fcc3d60108e149b4be3e15
    Former-commit-id: 1e6668a78850e77d6a3d9906aae884446e53dc9a

---
+++ examples/water/fugaku_strong_scale_48x1_small/sub.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# pjsub run_compress_preprocess_48x1x5_864x5.sh
+# pjsub run_compress_preprocess_48x1x11_864x5.sh
+# pjsub run_compress_preprocess_48x1x25_864x5.sh
+# pjsub run_compress_preprocess_48x1x57_864x5.sh
+# pjsub run_compress_preprocess_48x1x114_864x5.sh
+# pjsub run_compress_preprocess_48x1x228_864x5.sh
+# pjsub run_compress_preprocess_48x1x456_864x5.sh
