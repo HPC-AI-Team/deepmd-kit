e24c1d784100e89b0c388f0adcf42464f2ba4daf
gzq <gzq9425@qq.com>
2021-08-02 14:15:04 +0900

    water stong scale
    Former-commit-id: b5eb168c3858928b1a465c192e7e4aaacf7d1f01
    Former-commit-id: 5aad5d60ecc620601f41c8f7da2917b7e157f25a
    Former-commit-id: 2f0b58afa470401ed7bd5f4c82e88970cd38642e
    Former-commit-id: f47f7e2f23825bd5f5bd2650badd31df45ce3001
    Former-commit-id: 99248840d5efbd2a1c559cae873798a45c83480e
    Former-commit-id: dd34f5f4c868346b1f363c42d49b859c0ec028ca

--- examples/water/fugaku_strong_scale_24x2/sub.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# pjsub run_compress_preprocess_24x2x50_864x50.sh
-# pjsub run_compress_preprocess_24x2x110_864x50.sh
-# pjsub run_compress_preprocess_24x2x250_864x50.sh
-# pjsub run_compress_preprocess_24x2x570_864x50.sh
-# pjsub run_compress_preprocess_24x2x1140_864x50.sh
-# pjsub run_compress_preprocess_24x2x2280_864x50.sh
-# pjsub run_compress_preprocess_24x2x4560_864x50.sh
