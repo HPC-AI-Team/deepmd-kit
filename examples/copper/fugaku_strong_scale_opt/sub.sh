18eaf02a50ac99f90933a004f07eae8fc5f25a43
gzq <gzq9425@qq.com>
2021-08-02 18:22:16 +0900

    copper strong scale
    Former-commit-id: c1b160ca4eca2853e5530e69af3bbb93278ed3bb
    Former-commit-id: 04479cc89a42e2bd87043f3e8df83a24d4f9b93c
    Former-commit-id: 72b55ca85fdc138f1d95d200c69d43c555a4084f
    Former-commit-id: 17c47f86dcc19c8ba8e6b16ecc719845c971cba2
    Former-commit-id: 8d55ad41bf9442ea8c45573da805873fd9c25865
    Former-commit-id: 4b3d098963c238e072bfe64ec0401099fecf5af7

---
+++ examples/copper/fugaku_strong_scale_opt/sub.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+# pjsub run_compress_preprocess_8x6x20_1008x20.sh
+# pjsub run_compress_preprocess_8x6x40_1008x20.sh
+# pjsub run_compress_preprocess_8x6x75_1008x20.sh
+# pjsub run_compress_preprocess_16x3x150_1008x20.sh
+# pjsub run_compress_preprocess_16x3x285_1008x20.sh
+# pjsub run_compress_preprocess_16x3x570_1008x20.sh
+# pjsub run_compress_preprocess_16x3x1140_1008x20.sh
+# pjsub run_compress_preprocess_16x3x2280_1008x20.sh
+pjsub run_compress_preprocess_24x2x4560_1008x20.sh
