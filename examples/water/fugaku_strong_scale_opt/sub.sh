d9397e894c447d5c4727fba3d0830ad1e925b565
gzq <gzq9425@qq.com>
2021-08-04 11:29:34 +0900

    strong scale
    Former-commit-id: 350f17ffdf752cd478aa1379111ba4cea8bc9db3
    Former-commit-id: bc4115a879daf39c1c83d4d6c1368bc8b84022c7
    Former-commit-id: dc45a083a18d2317e49923df7fcf090dbfde1ff1
    Former-commit-id: d92050b5b43faf273e0368d96cb35cfd1bca25e2
    Former-commit-id: 96ee6b0feb74f9107bcd86b744fc0877ccc97171
    Former-commit-id: 2bc5cfef2479af8ef1a49402e2cb2dc42a7117f7

--- examples/water/fugaku_strong_scale_opt/sub.sh
+++ examples/water/fugaku_strong_scale_opt/sub.sh
@@ -1,10 +1,11 @@
 
 
-# pjsub run_compress_preprocess_8x6x50_864x50.sh
+pjsub run_compress_preprocess_8x6x50_864x50.sh
+pjsub run_compress_preprocess_8x6x110_864x50.sh
 # pjsub run_compress_preprocess_12x4x110_864x50.sh
 # pjsub run_compress_preprocess_16x3x110_864x50.sh
 # pjsub run_compress_preprocess_16x3x250_864x50.sh
 # pjsub run_compress_preprocess_16x3x250_864x50.sh
 # pjsub run_compress_preprocess_16x3x1140_864x50.sh
 # pjsub run_compress_preprocess_24x2x2280_864x50.sh
-pjsub run_compress_preprocess_24x2x4560_864x50.sh
+# pjsub run_compress_preprocess_24x2x4560_864x50.sh
