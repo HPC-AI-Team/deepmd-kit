f75aaf027d0e0992c44f760fbb56c182c3bfe6eb
gzq <gzq9425@qq.com>
2021-08-20 15:18:19 +0900

    tanh exp
    Former-commit-id: 0c15a19e5289be4c0bd3c55bea5ebb289efe7971
    Former-commit-id: 66a00e5b3b4137a36b6e2b8a1f3ac62eaf46c855
    Former-commit-id: 299bdabbfca3cf964ae79d4371c4b40b576d0cc1
    Former-commit-id: 27bc8267433e6cce02a9aa7f1dd4643e2671ea04
    Former-commit-id: dd20cf652d24e23d5fce50146a984eed6198dfba
    Former-commit-id: 1fc137ea58eb648909e452dd1176d5db0ba1dde1

--- source/lib/include/fast_tanh.h
+++ source/lib/include/fast_tanh.h
@@ -3,19 +3,20 @@
 #include <cmath>
 
 #include "poly_table_f.h"
+#include "poly_table_d.h"
 
 #define __min__(x,y) (x < y ? x : y)
 
-// #define poly_2_tanh_double(input,output)                             \
-//         double _x = (input);                                         \
-//         double _x_abs = fabs(_x);                                    \
-//         int _index = __min__(_x_abs * TABLE_LEN,TABLE_LEN * RANGE - 1);  \
-//         double _c0 = fast_tanh_poly_table_double[_index][0];         \
-//         double _c1 = fast_tanh_poly_table_double[_index][1];         \
-//         double _c2 = fast_tanh_poly_table_double[_index][2];         \
-//         double _y = ((_c0 * _x_abs) +_c1) * _x_abs + _c2;            \
-//         _y = _x < 0. ? -_y : _y;                                     \
-//         (output) = _y;  
+#define poly_2_tanh_double(input,output)                             \
+        double _x = (input);                                         \
+        double _x_abs = fabs(_x);                                    \
+        int _index = __min__(_x_abs * TABLE_LEN,TABLE_LEN * RANGE - 1);  \
+        double _c0 = fast_tanh_poly_table_double[_index][0];         \
+        double _c1 = fast_tanh_poly_table_double[_index][1];         \
+        double _c2 = fast_tanh_poly_table_double[_index][2];         \
+        double _y = ((_c0 * _x_abs) +_c1) * _x_abs + _c2;            \
+        _y = _x < 0. ? -_y : _y;                                     \
+        (output) = _y;  
 
 #define poly_2_tanh_float(input,output){                            \
         float _x = (input);                                         \
